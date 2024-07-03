1accbf1ec72cd7fb99c5ab71c68f8079fb23c1bb
Kathy-Ann Heitmeier <61460502+kheitm@users.noreply.github.com>
2024-07-03 11:35:03 +0200

    Update url_to_alias.sql

--- pre-processing/url_to_alias.sql
+++ pre-processing/url_to_alias.sql
@@ -18,24 +18,24 @@ event_destination_url=REGEXP_REPLACE(
 					REGEXP_REPLACE(
 					REGEXP_REPLACE(
 					REGEXP_REPLACE(
-					REGEXP_REPLACE(event_destination_url,'^https://365datascience.com/$','Homepage')
-	    ,'https://365datascience.com/signup(?:\/.*)?$','Sign up')
-        ,'https://365datascience.com/resources-center(?:\/.*)?$','Resources center')
-        ,'https://365datascience.com/courses(?:\/.*)?$','Courses')
-        ,'https://365datascience.com/career-tracks(?:\/.*)?$','Career tracks')
-        ,'https://365datascience.com/upcoming-courses(?:\/.*)?$','Upcoming courses')
-        ,'https://365datascience.com/career-track-certificate(?:\/.*)?$','Career track certificate')
-        ,'https://365datascience.com/success-stories(?:\/.*)?$','Success stories')
-        ,'https://365datascience.com/blog(?:\/.*)?$','Blog')
-        ,'https://365datascience.com/pricing(?:\/.*)?$','Pricing')
-        ,'https://365datascience.com/about-us(?:\/.*)?$','About us')
-        ,'https://365datascience.com/course-certificate(?:\/.*)?$','Course certificate')
-        ,'https://365datascience.com/instructors(?:\/.*)?$','Instructors')
-        ,'https://365datascience.com/checkout(?:\/.*)?$','Checkout')
-        ,'https://365datascience.com/login(?:\/.*)?$','Log in');
+					REGEXP_REPLACE(event_destination_url,'^example.com/$','Homepage')
+	    ,'example.com/signup(?:\/.*)?$','Sign up')
+        ,'example.com/resources-center(?:\/.*)?$','Resources center')
+        ,'example.com/courses(?:\/.*)?$','Courses')
+        ,'example.com/career-tracks(?:\/.*)?$','Career tracks')
+        ,'example.com/upcoming-courses(?:\/.*)?$','Upcoming courses')
+        ,'example.com/career-track-certificate(?:\/.*)?$','Career track certificate')
+        ,'example.com/success-stories(?:\/.*)?$','Success stories')
+        ,'example.com/blog(?:\/.*)?$','Blog')
+        ,'example.com/pricing(?:\/.*)?$','Pricing')
+        ,'example.com/about-us(?:\/.*)?$','About us')
+        ,'example.com/course-certificate(?:\/.*)?$','Course certificate')
+        ,'example.com/instructors(?:\/.*)?$','Instructors')
+        ,'example.com/checkout(?:\/.*)?$','Checkout')
+        ,'example.com/login(?:\/.*)?$','Log in');
 
 -- Use a single query to replace the remaining URLS with Other
-SET  event_destination_url=REGEXP_REPLACE(event_destination_url,'https://365datascience.com(?:\/.*)?$','Other');
+SET  event_destination_url=REGEXP_REPLACE(event_destination_url,'example.com(?:\/.*)?$','Other');
 
 SET SQL_SAFE_UPDATES = 1;
 
