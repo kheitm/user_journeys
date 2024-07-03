8dee28a926709f56f31c6d5c817b44d99965b700
Kathy-Ann Heitmeier <61460502+kheitm@users.noreply.github.com>
2024-07-03 11:40:19 +0200

    Add files via upload

---
+++ pre-processing/reformat_purchase_type.sql
@@ -0,0 +1,24 @@
+-- Change subcription type to text description
+
+USE user_journey_data;
+
+
+-- Change column data type from tinyint to varchar
+ALTER TABLE student_purchases MODIFY COLUMN purchase_type varchar(50);
+DESCRIBE student_purchases;
+
+
+-- replace number strings with full text
+SET SQL_SAFE_UPDATES = 0;
+
+UPDATE user_journey_data.student_purchases 
+
+
+SET  
+purchase_type=REPLACE(REPLACE(REPLACE(purchase_type, 0, 'monthly'), 1, 'quarterly'), 2, 'annually');
+					
+SET SQL_SAFE_UPDATES = 1;
+
+
+
+
